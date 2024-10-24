33a1cf0cf5401f2759ab5bd141fc8074a387882b
Mehmet Emin Ozkan <117169042+MehmetEminOzkan@users.noreply.github.com>
2024-10-24 10:26:16 +0300

    Update contact-list-app.py

--- contact-list-app.py
+++ contact-list-app.py
@@ -10,8 +10,8 @@ db_endpoint = open("/home/ec2-user/terraform-proje/dbserver.endpoint", 'r', enco
 # Configure mysql database
 
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
-app.config['MYSQL_DATABASE_USER'] = 'techpro'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'techpro123'
+app.config['MYSQL_DATABASE_USER'] = 'terraform'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'trproje123'
 app.config['MYSQL_DATABASE_DB'] = 'contactlist'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
